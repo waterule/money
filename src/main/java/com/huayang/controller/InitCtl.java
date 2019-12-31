faa8dd7aa118cc20f710ce7ca5584777c6ae4d41
lhy <waterule@163.com>
2019-12-31 12:57:43 +0800

    logback   info level

--- src/main/java/com/huayang/controller/InitCtl.java
+++ src/main/java/com/huayang/controller/InitCtl.java
@@ -1,7 +1,17 @@
 package com.huayang.controller;
 
 import lombok.Data;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Data
+@Log
+@RestController
 public class InitCtl {
+    @GetMapping("/")
+    public String hello(){
+        log.info("------------2020---------");
+        return "hello,2020";
+    }
 }
