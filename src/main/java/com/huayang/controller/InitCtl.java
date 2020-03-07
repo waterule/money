9c23ca00269a8d0308c4a1c330336dcd879b53be
liuhaiyang <waterule@163.com>
2020-03-07 22:31:44 +0800

    支付系统

--- src/main/java/com/huayang/controller/InitCtl.java
+++ src/main/java/com/huayang/controller/InitCtl.java
@@ -1,17 +1,16 @@
 package com.huayang.controller;
 
-import lombok.Data;
 import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Log
 @RestController
 public class InitCtl {
     @GetMapping("/")
-    public String hello(){
-        log.info("------------2020---------");
-        return "hello,2020";
+    public ModelAndView  hello(){
+        return new ModelAndView(" index.html");
+
     }
 }
